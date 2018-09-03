8ff78f897032f6ef5d72bf36ae1857a15c497aaa
verossi <40401058+verossi@users.noreply.github.com>
2018-09-03 10:53:44 +0200

    Add files via upload

---
+++ Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.d
@@ -0,0 +1,104 @@
+Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.o: \
+ ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Inc/stm32f4xx_hal_conf.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Inc/main.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/CMSIS/Include/core_cm4.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/CMSIS/Include/core_cmSimd.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_hcd.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Inc/stm32f4xx_hal_conf.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Inc/main.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/CMSIS/Include/core_cm4.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/CMSIS/Include/core_cmSimd.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_hcd.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h:
---
+++ Debug/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_ctlreq.d
@@ -0,0 +1,125 @@
+Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_ctlreq.o: \
+ ../Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_ctlreq.c \
+ C:/SystemWorkbench/workspace/trial1_timer/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_ctlreq.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_core.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Inc/usbh_conf.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/CMSIS/Include/core_cm4.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/CMSIS/Include/core_cmSimd.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Inc/stm32f4xx_hal_conf.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Inc/main.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_hcd.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_def.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_ioreq.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_pipes.h \
+ C:/SystemWorkbench/workspace/trial1_timer/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_ctlreq.h
+
+C:/SystemWorkbench/workspace/trial1_timer/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_ctlreq.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_core.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Inc/usbh_conf.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/CMSIS/Include/core_cm4.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/CMSIS/Include/core_cmSimd.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Inc/stm32f4xx_hal_conf.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Inc/main.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_hcd.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_def.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_ioreq.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_pipes.h:
+
+C:/SystemWorkbench/workspace/trial1_timer/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_ctlreq.h:
